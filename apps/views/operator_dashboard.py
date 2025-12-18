85cc5dca3f24204223a8b5b8b4fcc5f8f0d6020b
Rahmatillo Qodirov <kadirovrahmatilloqodirov202@gmail.com>
2025-12-18 11:38:57 +0500

    qodirov

--- apps/views/operator_dashboard.py
+++ apps/views/operator_dashboard.py
@@ -1,17 +1,18 @@
+from datetime import datetime, time
+from django.utils.timezone import localtime, now
 from django.db.models import Count, Q, Sum
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
 from apps.models.operator import Operator
 from apps.models.leads import Lead
-from datetime import datetime, time
-from django.utils.timezone import localtime, now
 
 class OperatorsDashboardAPIView(APIView):
     def get(self, request):
+        # Oy boshini aniqlaymiz
         today = localtime(now()).date()
         month_start = datetime.combine(today.replace(day=1), time.min)
 
+        # Operatorlarni olish + lead va sold_leads
         operators = Operator.objects.select_related("user").annotate(
             total_leads=Count(
                 "leads",
@@ -38,13 +39,14 @@ class OperatorsDashboardAPIView(APIView):
             conversion = round((sold / total) * 100, 2) if total else 0
             conversions.append(conversion)
 
-            # Daromad: real sotilgan leadlarning price bo'yicha
+            # Daromad (oy boshidan hozirgi kungacha)
+
             income = Lead.objects.filter(
                 operator=operator,
                 status=Lead.Status.SOLD,
                 created_at__gte=month_start
             ).aggregate(
-                total_income=Sum('price')
+                total_income=Sum('course__price')
             )['total_income'] or 0
 
             operator_name = operator.user.get_full_name().strip()
@@ -58,15 +60,15 @@ class OperatorsDashboardAPIView(APIView):
                 "daromad": income
             })
 
+        # Shu yerga qo‘shamiz
+        oylik_leadlar = Lead.objects.filter(created_at__gte=month_start).count()
+
         return Response({
             "cards": {
-                "jami_operatorlar": Operator.objects.count(),
+                "oylik_leadlar": oylik_leadlar,
+                "oylik_daromad": sum([op['daromad'] for op in operators_data]),
                 "aktiv_operatorlar": Operator.objects.filter(user__is_active=True).count(),
-                "jami_sotilgan": Lead.objects.filter(
-                    status=Lead.Status.SOLD,
-                    created_at__gte=month_start
-                ).count(),
-                "oylik_konversiya": round(sum(conversions) / len(conversions), 2) if conversions else 0
+                "oylik_konversiya": round(sum(conversions)/len(conversions), 2) if conversions else 0
             },
             "operator_samaradorligi": operators_data
         })
